009e43c055b49d361ed4906bdd65782c8a499270
Miroslava <m.marchuk@kitsoft.kiev.ua>
2020-11-24 11:47:53 +0200

    added routs

--- source/navigation/App.js
+++ source/navigation/App.js
@@ -1,17 +1,26 @@
 // Core
 import React, { Component } from 'react';
 import { hot } from 'react-hot-loader';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 // Pages
-import { Feed } from '../pages';
+import { Login, Signup, Feed, Profile, NewPassword } from '../pages';
+
+// Instruments
+import { book } from './book';
 
 @hot(module)
 export default class App extends Component {
     render () {
         return (
-            <>
-                <Feed />
-            </>
+            <Switch>
+               <Route component={Login} path={book.login} />
+               <Route component={Signup} path={book.signUp} />
+               <Route component={Feed} path={book.feed} />
+               <Route component={Profile} path={book.profile} />
+               <Route component={NewPassword} path={book.newPassword} />
+               <Redirect to={book.login} />
+            </Switch>
         );
     }
 }
